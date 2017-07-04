6b9ffb7754cf6749b40b889164014046a649f788
Luke McMahon <lukemcmahon96@gmail.com>
2017-07-04 18:36:03 +1000

    Made folders and started on Queues and Stacks

---
+++ Main/Stack/Queue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms.Stack
+{
+    class Queue<T>
+    {
+
+        int maxSize = 8;
+        T[] items;
+        int head = -1;
+        int tail = -1;
+        int numOfItems = 0;
+
+        public Queue()
+        {
+
+        }
+
+        public Queue(T item)
+        {
+            if(IsFull())
+            {
+                throw new Exception("Queue is full");
+            }
+            if(tail == items.Length - 1)
+            {
+                tail = -1;
+            }
+
+            items[++tail] = item;
+            numOfItems++;
+        }
+
+        public T Peek()
+        {
+            return items[head + 1];
+        }
+
+        public void Enqueue(T data)
+        {
+            if (IsFull())
+                throw new Exception("Queue is full");
+
+            if(tail == items.Length-1)
+            {
+                tail = -1;
+            }
+
+            items[++tail] = data;
+            numOfItems++;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new Exception("Queue is empty");
+            if(head == items.Length - 1)
+            {
+                head = -1;
+            }
+            numOfItems--;
+            return items[++head];
+        }
+        
+        private bool IsFull()
+        {
+            return numOfItems == items.Length;
+        }
+
+        private bool IsEmpty()
+        {
+            return numOfItems == 0;
+        }
+    }
+}
---
+++ Main/Stack/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms.Stack
+{
+    class Stack<T>
+    {
+        int maxSize = 9;
+        T[] stackArray;
+        int top = -1;
+
+        public void Init()
+        {
+            stackArray = new T[maxSize];
+        }
+        
+        public void Push(T data)
+        {
+            top++;
+            stackArray[top] = data;
+        }
+
+        public T Pop()
+        {
+            T result = stackArray[top];
+            top--;
+            return result;
+        }
+
+        public T Peek()
+        {
+            return stackArray[top];
+        }
+    }
+}

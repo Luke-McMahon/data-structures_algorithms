965d2bd35b05caf5fa405f57a63339d0c9aeb258
Luke McMahon <lukemcmahon96@gmail.com>
2017-07-04 12:10:21 +1000

    Initial Commit
    + Working on random sorting algorithms, learning how to actually write them.

---
+++ Main/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int a = rand.Next(7, 15);
+            int[] arr = new int[a];
+            for(int i = 0; i < a; i++)
+            {
+                arr[i] = rand.Next(20);
+            }
+            Console.WriteLine("BubbleSort");
+            Sort.BubbleSort(arr);
+
+            Console.WriteLine();
+
+            Console.WriteLine("InsertionSort");
+            Sort.InsertionSort(arr);
+
+            Console.WriteLine();
+
+            Console.WriteLine("SelectionSort");
+            Sort.SelectionSort(arr);
+
+        }
+    }
+}
---
+++ Main/Sorting/Sort.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms
+{
+    class Sort
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            Console.WriteLine("Unsorted: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+            Console.WriteLine();
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for(int j = 0; j < arr.Length - 1; j++)
+                {
+                    if(arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void SelectionSort(int[] arr)
+        {
+            Console.WriteLine("Unsorted: ");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+
+            Console.WriteLine();
+            
+            if (arr.Length > 0)
+            {
+                for (int i = 0; i < arr.Length - 2; i++)
+                {
+                    int minIndex = i;
+                    for (int j = i + 1; j < arr.Length - 1; j++)
+                    {
+                        if (arr[j] < arr[minIndex])
+                            minIndex = j;
+                    }
+                    int temp = arr[minIndex];
+                    arr[minIndex] = arr[i];
+                    arr[i] = temp;
+                }
+
+                Console.WriteLine("Sorted: ");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(" " + arr[i]);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void InsertionSort(int[] arr)
+        {
+            Console.WriteLine("Unsorted: ");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+            Console.WriteLine();
+
+            for(int i = 0; i <= arr.Length - 1; i++)
+            {
+
+
+                int current = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > current)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = current;
+            }
+
+            Console.WriteLine("Sorted: ");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
+
